5230ff4b35fe26c518ac967406110b6e6b64dabc
Jeremy Zelaya <jzelaya@krames.com>
2023-08-24 22:44:57 -0600

    feat: connect to DB

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use zero2prod::run;
+use std::net::TcpListener;
+
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener =
-        std::net::TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to an address");
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,6 @@
 use reqwest::Client;
+use sqlx::{Connection, PgConnection};
+use zero2prod::{configuration::get_configuration, startup};
 
 #[tokio::test]
 async fn health_check_works() {
@@ -16,11 +18,69 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    println!("{}", connection_string);
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+    let client = Client::new();
+    let body = "name=Jeremy%20Zelaya&email=jeremyzelaya%40example.com";
+    // Act
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    assert_eq!(saved.email, "jeremyzelaya@example.com");
+    assert_eq!(saved.name, "Jeremy Zelaya");
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let address = spawn_app();
+    let client = Client::new();
+    let test_cases = vec![
+        ("name=Jeremy%20Zelaya", "missing email"),
+        ("email=john%40example.com", "missing name"),
+        ("", "missing name and email"),
+    ];
+    // Act
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with an invalid request body {}.",
+            error_message
+        );
+    }
+}
+
 fn spawn_app() -> String {
     let listener =
         std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind to address");
+    let server = startup::run(listener).expect("Failed to bind to address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
