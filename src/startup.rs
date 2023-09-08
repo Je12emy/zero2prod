bb3aad725bae78e030cbd691371c5aaceab17495
Jeremy <jeremyzelaya@hotmail.es>
2023-09-08 01:19:53 -0600

    feat: add log middleware

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::{dev::Server, web, App, HttpServer, middleware::Logger};
 use sqlx::PgPool;
 
 use crate::routes;
@@ -9,6 +9,7 @@ pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std
     let connection = web::Data::new(connection_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
             .app_data(connection.clone())
