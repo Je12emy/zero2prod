59af5ff59d7e377b75c919c0ce4b179a1aa13030
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2023-10-07 15:09:28 -0600

    feat: integrate quickcheck for test

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -26,27 +26,36 @@ mod tests {
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
 
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
     #[test]
-    fn empty_string_is_rejected(){
+    fn empty_string_is_rejected() {
         let email = "".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
 
     #[test]
-    fn email_missing_at_symbol_is_rejected(){
+    fn email_missing_at_symbol_is_rejected() {
         let email = "ursuladomain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
 
     #[test]
-    fn email_missing_subject_is_rejected(){
+    fn email_missing_subject_is_rejected() {
         let email = "@domain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
 
-    #[test]
-    fn valid_emails_are_parsed_successfully() {
-        let email = SafeEmail().fake();
-        claim::assert_ok!(SubscriberEmail::parse(email));
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
