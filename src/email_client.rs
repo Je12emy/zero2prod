51d74569d9b3bcf5d3c8833d038e7051a860e7e0
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2023-10-20 23:05:15 -0600

    feat: draft send email request

--- src/email_client.rs
+++ src/email_client.rs
@@ -27,13 +27,37 @@ impl EmailClient {
         html_content: &str,
         text_content: &str,
     ) -> Result<(), String> {
+        let url = format!("{}/email", self.base_url);
+        let request_body = SendEmailRequest {
+            from: self.sender.as_ref().to_owned(),
+            to: recipent.as_ref().to_owned(),
+            subject: subject.to_owned(),
+            html_body: html_content.to_owned(),
+            text_body: text_content.to_owned(),
+        };
+        let builder = self.http_client.post(&url).json(&request_body);
         Ok(())
     }
 }
 
+#[derive(serde::Serialize)]
+struct SendEmailRequest {
+    from: String,
+    to: String,
+    subject: String,
+    html_body: String,
+    text_body: String,
+}
+
 #[cfg(test)]
 mod tests {
-    use fake::{faker::{internet::en::SafeEmail, lorem::en::{Sentence, Paragraph}}, Fake};
+    use fake::{
+        faker::{
+            internet::en::SafeEmail,
+            lorem::en::{Paragraph, Sentence},
+        },
+        Fake,
+    };
     use wiremock::{matchers::any, Mock, MockServer, ResponseTemplate};
 
     use crate::domain::SubscriberEmail;
