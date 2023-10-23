7d76f12ac075971447be29436dd8f2fc69bbecdd
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2023-10-22 20:53:33 -0600

    feat: timeout config

--- src/configuration.rs
+++ src/configuration.rs
@@ -18,7 +18,8 @@ pub struct Settings {
 pub struct EmailClientSettings {
     pub base_url: String,
     pub sender_email: String,
-    pub authorization_token: Secret<String>
+    pub authorization_token: Secret<String>,
+    pub timeout_milliseconds: u64,
 }
 
 #[derive(serde::Deserialize)]
@@ -107,6 +108,11 @@ impl TryFrom<String> for Environment {
 }
 
 impl EmailClientSettings {
+
+    pub fn timeout(&self) -> std::time::Duration {
+        std::time::Duration::from_millis(self.timeout_milliseconds)
+    }
+
     pub fn sender(&self) -> Result<SubscriberEmail, String> {
         SubscriberEmail::parse(self.sender_email.clone())
     }
