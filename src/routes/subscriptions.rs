736a871ab02212a1d890057768cb6990fd89aaee
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2023-10-29 22:47:09 -0600

    fix: extra value in insert statement

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -40,7 +40,7 @@ pub async fn insert_subscriber(
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-            INSERT INTO subscriptions (id, email, name, subscribed_at)
+            INSERT INTO subscriptions (id, email, name, subscribed_at, status)
             VALUES ($1, $2, $3, $4, 'confirmed')
         "#,
         Uuid::new_v4(),
